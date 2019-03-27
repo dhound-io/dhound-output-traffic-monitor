0fb153ed31bced6c6b705b8d89657c296d9b5511
WarGot <wargot@gmail.com>
2019-03-27 14:44:22 +0300

    Update options.go

--- options.go
+++ options.go
@@ -15,7 +15,7 @@ type Options struct {
 
 func (options *Options) ParseArguments() {
 
-	flag.StringVar(&options.LogFile, "log-file", "syslog", "network events output: syslog, console, <path to a custom file>; default: console")
+	flag.StringVar(&options.LogFile, "log-file", "console", "network events output: syslog, console, <path to a custom file>; default: console")
 	flag.StringVar(&options.NetworkInterface, "eth", options.NetworkInterface, "listen to a particular network interface; default: listen to all active network interfaces")
 	flag.BoolVar(&options.Verbose, "verbose", options.Verbose, "log more detailed and debug information; default: false")
 	flag.BoolVar(&options.Version, "version", options.Version, "dhound output traffic monitor version")
