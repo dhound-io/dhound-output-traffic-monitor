93ddcc6023cd0fe9834960e1c233c4f37fef4204
WarGot <wargot@gmail.com>
2019-03-29 13:54:18 +0300

    DHD-617

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.1.23
+VERSION=1.1.24
 PACKAGE_NAME=dhound-output-traffic-monitor
 
 .PHONY: default
--- netstat.go
+++ netstat.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"time"
-	net "github.com/shirou/gopsutil/net"
 )
 
 type NetStatInfo struct {
@@ -25,7 +24,7 @@ func (netstat *NetStatManager) Run() {
 	go func() {
 		for {
 			netstat.SyncPortList()
-			time.Sleep(1 * time.Second)
+			time.Sleep(2 * time.Second)
 		}
 	}()
 }
@@ -47,55 +46,5 @@ func (netstat *NetStatManager) FindNetstatInfoByLocalPort(localIp string, localP
 			}
 		}
 	}
-
 	return nil
-}
-
-func (manager *NetStatManager) SyncPortList() {
-	connections, err := net.Connections(manager.Options.Protocol)
-	if err != nil {
-		emitLine(logLevel.verbose,"Unknown protocol:"+manager.Options.Protocol)
-		return
-	}
-
-	currentTimeNumber := time.Now().UTC().Unix()
-	// keep info about pid only 60 seconds
-	minTime := currentTimeNumber - 60
-
-	var list []*NetStatInfo
-	for _, stat := range manager._cache {
-		if stat.EventTimeUtcNumber > minTime {
-			list = append(list, stat)
-		}
-	}
-
-	if list == nil {
-		list = manager._cache
-	}
-
-	for _, connection := range connections {
-		 if connection.Laddr.Port > 0 && connection.Pid > 0 && connection.Laddr.IP != "127.0.0.1" && connection.Laddr.IP != "::1" {
-			isFound := false
-			for _, stat := range list {
-				if stat.LocalIp == connection.Laddr.IP && stat.LocalPort == connection.Laddr.Port {
-					stat.Pid = connection.Pid
-					isFound = true
-				}
-			}
-
-			if !isFound {
-				stat := &NetStatInfo{
-					Pid:                connection.Pid,
-					LocalIp:            connection.Laddr.IP,
-					LocalPort:          connection.Laddr.Port,
-					EventTimeUtcNumber: currentTimeNumber,
-				}
-
-				list = append(list, stat)
-				// debugJson(stat)
-			}
-		}
-	}
-
-	manager._cache = list
 }
