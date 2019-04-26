c5ae955f282e4a789b23a2c6995be3d4ed7a8674
WarGot <wargot@gmail.com>
2019-04-26 14:23:49 +0300

    Removed unnecessary

--- publish/build-debian.sh
+++ publish/build-debian.sh
@@ -3,19 +3,10 @@
 # check official debian ports https://www.debian.org/ports/
 
 cd ..
-#https://github.com/golang/go/wiki/GoArm
-
-env GOOS=linux GOARCH=arm GOARM=5 ARCHITECTURE=armel make deb
-env GOOS=linux GOARCH=arm ARCHITECTURE=arm make deb
-env GOOS=linux GOARCH=arm ARCHITECTURE=armhf make deb
-env GOOS=linux GOARCH=arm GOARM=6 ARCHITECTURE=armv6hl make deb
-env GOOS=linux GOARCH=arm GOARM=7 ARCHITECTURE=armv7hl make deb
-env GOOS=linux GOARCH=arm64 ARCHITECTURE=arm64 make deb
-
 
 env CC=gcc GOOS=linux GOARCH=386 CGO_ENABLED=1 GOGCCFLAGS="-fPIC -m32 -fmessage-length=0" CGO_LDFLAGS="-L/dhound-build-server/dhound-output-traffic-monitor/publish/libpcap/i386"  ARCHITECTURE=i386 make deb
 env CC=gcc GOOS=linux GOARCH=amd64 CGO_ENABLED=1 GOGCCFLAGS="-fPIC -m64 -fmessage-length=0" ARCHITECTURE=amd64  make deb
 env CC=gcc GOOS=linux GOARCH=amd64 CGO_ENABLED=1 GOGCCFLAGS="-fPIC -m64 -fmessage-length=0" ARCHITECTURE=ia64 make deb
 
-mv *.deb publish/packages
+mv *.deb publish/pakages
 rm dhound-output-traffic-monitor
--- publish/clean.sh
+++ publish/clean.sh
@@ -2,5 +2,8 @@
 rm -r *.deb
 rm -r *.rpm
 
+rm -r ../*.deb
+rm -r ../*.rpm
+
 rm -r packages/*.deb
 rm -r packages/*.rpm
