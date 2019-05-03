1e73d3c9aabeb88ab77d8e0b9894c50636c6fa6a
Denis Koloshko <d.koloshko@gmail.com>
2019-05-03 11:28:41 +0300

    netstat optimization

--- netstat.go
+++ netstat.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"time"
+
+	"github.com/shirou/gopsutil/net"
+)
+
 type NetStatInfo struct {
 	Pid                int32
 	LocalIp            string
@@ -8,7 +14,7 @@ type NetStatInfo struct {
 }
 
 type NetStatManager struct {
-	_cache []*NetStatInfo
+	_cache  []*NetStatInfo
 	Options *Options
 }
 
@@ -16,3 +22,84 @@ func (netstat *NetStatManager) Init() {
 	netstat._cache = make([]*NetStatInfo, 0)
 }
 
+func (netstat *NetStatManager) Run() {
+
+}
+
+func (manager *NetStatManager) SyncPortList() {
+	connections, err := net.Connections("all")
+	if err != nil {
+		return
+	}
+
+	currentTimeNumber := time.Now().UTC().Unix()
+	// keep info about pid only particular seconds
+	minTime := currentTimeNumber - 60*3
+
+	var list []*NetStatInfo
+	for _, stat := range manager._cache {
+		if stat.EventTimeUtcNumber > minTime {
+			list = append(list, stat)
+		}
+	}
+
+	if list == nil {
+		list = manager._cache
+	}
+
+	for _, connection := range connections {
+		if connection.Laddr.Port > 0 && connection.Laddr.IP != "127.0.0.1" && connection.Laddr.IP != "::1" {
+			isFound := false
+			for _, stat := range list {
+				if stat.LocalIp == connection.Laddr.IP && stat.LocalPort == connection.Laddr.Port {
+					stat.Pid = connection.Pid
+					isFound = true
+				}
+			}
+
+			if !isFound {
+				stat := &NetStatInfo{
+					Pid:                connection.Pid,
+					LocalIp:            connection.Laddr.IP,
+					LocalPort:          connection.Laddr.Port,
+					EventTimeUtcNumber: currentTimeNumber,
+				}
+
+				list = append(list, stat)
+				debugJson(stat)
+			}
+		}
+	}
+
+	manager._cache = list
+}
+
+func (netstat *NetStatManager) FindNetstatInfoByLocalPort(localIp string, localPort uint32) *NetStatInfo {
+	result := InternalFindNetstatInfoByLocalPort(localIp, localPort)
+	if result == nil {
+		netstat.SyncPortList()
+		result := InternalFindNetstatInfoByLocalPort(localIp, localPort)
+	}
+
+	return result
+}
+
+func (netstat *NetStatManager) InternalFindNetstatInfoByLocalPort(localIp string, localPort uint32) *NetStatInfo {
+	cache := netstat._cache
+	if len(cache) > 0 {
+		// check by ip and port
+		for _, info := range cache {
+			if info.LocalIp == localIp && info.LocalPort == localPort {
+				return info
+			}
+		}
+
+		// check only by port
+		for _, info := range cache {
+			if info.LocalPort == localPort {
+				return info
+			}
+		}
+	}
+	return nil
+}
--- netstat_windows.go
+++ netstat_windows.go
@@ -1,4 +1,5 @@
-// +build windows
+// +build ignore
+
 package main
 
 import (
